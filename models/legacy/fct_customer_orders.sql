a4b3cd5b9945b16cc1dea84227e55a8821371b1d
trevor@infostrux.com <trevor@infostrux.com>
2023-02-23 20:02:09 +0000

    import CTE's at the top

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,4 +1,29 @@
 with 
+
+-- Import CTE's
+
+customers as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+orders as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+payments as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+), 
+
+-- Logical CTE's 
+-- Final CTE
+-- Simple Select Statement
+
 paid_orders as (
         select 
             orders.id as order_id,
@@ -9,17 +34,17 @@ paid_orders as (
             p.payment_finalized_date,
             c.first_name    as customer_first_name,
             c.last_name as customer_last_name
-        from {{ source('jaffle_shop', 'orders') }} as orders
+        from orders
         left join (
             select 
                     orderid as order_id, 
                     max(created) as payment_finalized_date, 
                     sum(amount) / 100.0 as total_amount_paid
-            from {{ source('stripe', 'payment') }}
+            from payments
             where status <> 'fail'
             group by 1
         ) p on orders.id = p.order_id
-        left join {{ source('jaffle_shop', 'customers') }} c on orders.user_id = c.id ),
+        left join customers c on orders.user_id = c.id ),
 
 customer_orders as (
     select 
@@ -27,8 +52,8 @@ customer_orders as (
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(orders.id) as number_of_orders
-    from {{ source('jaffle_shop', 'customers') }} c 
-    left join {{ source('jaffle_shop', 'orders') }} as orders on orders.user_id = c.id 
+    from customers  as c 
+    left join orders on orders.user_id = c.id 
     group by 1)
 
 select
