ea9dd686303ed8725692ed23d29238143e8d9855
trevor@infostrux.com <trevor@infostrux.com>
2023-02-23 19:54:28 +0000

    reload legacy file, consistent spacing, lowercase

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,63 +1,37 @@
 with 
-    
-    -- import cte's
-    orders as (
-        select * from {{ source('jaffle_shop', 'orders') }}
-    ),
-
-    customers as (
-        select * from {{ source('jaffle_shop', 'customers') }}
-    ),
-
-    payments as (
-        select * from {{ source('stripe', 'payment') }}
-    ), 
-    
-    -- logical cte's
-    -- final cte
-    -- simple select statement
-    
-    
-    paid_orders as (
-        
+paid_orders as (
         select 
-            
             orders.id as order_id,
             orders.user_id	as customer_id,
             orders.order_date as order_placed_at,
             orders.status as order_status,
             p.total_amount_paid,
             p.payment_finalized_date,
-            customers.first_name as customer_first_name,
-            customers.last_name as customer_last_name
-        
-        from orders
+            c.first_name    as customer_first_name,
+            c.last_name as customer_last_name
+        from raw.jaffle_shop.orders as orders
         left join (
-                    select 
-                        orderid as order_id, 
-                        max(created) as payment_finalized_date, 
-                        sum(amount) / 100.0 as total_amount_paid
-                    from payments
-                    where status <> 'fail'
-                    group by 1) p on orders.id = p.order_id
-        left join customers on orders.user_id = customers.id ),
+            select 
+                    orderid as order_id, 
+                    max(created) as payment_finalized_date, 
+                    sum(amount) / 100.0 as total_amount_paid
+            from raw.stripe.payment
+            where status <> 'fail'
+            group by 1
+        ) p on orders.id = p.order_id
+        left join raw.jaffle_shop.customers c on orders.user_id = c.id ),
 
-    customer_orders as (
-        
-        select 
-            
-            customers.id as customer_id
-            , min(order_date) as first_order_date
-            , max(order_date) as most_recent_order_date
-            , count(orders.id) as number_of_orders
-       
-        from customers 
-        left join orders
-        on orders.user_id = customers.id 
-        group by 1)
+customer_orders as (
+    select 
+        c.id as customer_id
+        , min(order_date) as first_order_date
+        , max(order_date) as most_recent_order_date
+        , count(orders.id) as number_of_orders
+    from raw.jaffle_shop.customers c 
+    left join raw.jaffle_shop.orders as orders on orders.user_id = c.id 
+    group by 1)
 
 select
-    
     p.*,
     row_number() over (order by p.order_id) as transaction_seq,
     row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
@@ -66,16 +40,16 @@ select
     else 'return' end as nvsr,
     x.clv_bad as customer_lifetime_value,
     c.first_order_date as fdos
+from paid_orders p
+left join customer_orders as c using (customer_id)
+left join 
+(
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
     from paid_orders p
-    left join customer_orders as c using (customer_id)
-    left outer join 
-        (
-            select
-                p.order_id,
-                sum(t2.total_amount_paid) as clv_bad
-            from paid_orders p
-            left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-            group by 1
-            order by p.order_id
-        ) x on x.order_id = p.order_id
-    order by order_id
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+) x on x.order_id = p.order_id
+order by order_id
